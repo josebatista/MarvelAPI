2899a5ff9e14549e77586b06ae0c6ce774bdecf8
jpereira <josebatistapereira@gmail.com>
2019-12-09 23:12:46 -0300

    created detail activity.

---
+++ app/src/main/java/com/example/marvelapi/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.marvelapi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+
+}
